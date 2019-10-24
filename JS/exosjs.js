f289f5cf6422fb22dd83296edd1253a9402916f7
jmc <jeanmarc.cestrieres@gmail.com>
2019-10-24 16:58:00 +0200

    ajout modification du 24 oct 2019

---
+++ JS/exosjs.js
@@ -0,0 +1,10 @@
+var elmt = document.getElementById("texte1");
+
+elmt.style.color = "green";
+elmt.style.textAlign = "center";
+
+var elmt = document.getElementById("texte2");
+
+elmt.style.color = "blue";
+elmt.style.textAlign = "center";
+
